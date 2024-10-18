3df0b1f1e45d20d53e227ecb8a7d65301d8c501f
Maykal Tenev <michaeltenev@gmail.com>
2024-10-18 13:47:03 +0200

    01_Train

---
+++ 03_Arrays/03_Exercise/01_Train/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = int.Parse(Console.ReadLine());
+int[] train = new int[n];
+for (int i = 0; i < n; i++)
+{
+    train[i] = int.Parse(Console.ReadLine());
+}
+int total = 0;
+for (int i = 0; i < train.Length; i++)
+{
+    total += train[i];
+    Console.Write($"{train[i]} ");
+}
+Console.WriteLine();
+Console.WriteLine(total);
