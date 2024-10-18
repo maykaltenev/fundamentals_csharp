d12413051270e5c49342cff366cdc3bdd3235549
Maykal Tenev <michaeltenev@gmail.com>
2024-10-18 12:51:46 +0200

    05_SumEvenNumbers and 06_EvenAndOddSubstraction

---
+++ 03_Arrays/03_Labs/06_EvenAndOddSubstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿// string n = Console.ReadLine();
+// string[] inputStrings = n.Split(" ");
+
+int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int sumEven = 0;
+int sumOdd = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    // numbers[i] = int.Parse(inputStrings[i]);
+
+    if (numbers[i] % 2 == 0)
+    {
+        sumEven += numbers[i];
+    }
+    else
+    {
+        sumOdd += numbers[i];
+    }
+}
+
+Console.WriteLine(sumEven - sumOdd);
