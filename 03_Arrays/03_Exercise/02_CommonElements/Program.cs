27f5281e58a4249088e7cec68e4e37be9244ba4b
Maykal Tenev <michaeltenev@gmail.com>
2024-10-19 00:25:04 +0200

    02_CommonElements

---
+++ 03_Arrays/03_Exercise/02_CommonElements/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] first = Console.ReadLine().Split(" ");
+string[] second = Console.ReadLine().Split(" ");
+string[] common = new string[second.Length];
+int commonIndex = 0;
+for (int i = 0; i < first.Length; i++)
+{
+    for (int j = 0; j < second.Length; j++)
+    {
+        if (first[i] == second[j])
+        {
+            bool alreadyExists = false;
+            for (int k = 0; k < commonIndex; k++)
+            {
+                if (common[k] == first[i])
+                {
+                    alreadyExists = true;
+                    break;
+                }
+            }
+            if (!alreadyExists)
+            {
+                common[commonIndex] = first[i];
+                commonIndex++;
+            }
+        }
+    }
+}
+if (commonIndex > 0)
+{
+    for (int i = 0; i < commonIndex; i++)
+    {
+        Console.Write(common[i] + " ");
+    }
+    Console.WriteLine();
+}
