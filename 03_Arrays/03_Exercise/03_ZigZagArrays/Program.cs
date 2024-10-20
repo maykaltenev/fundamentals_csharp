847e1ec7a57b5e37a6f50f1e6f9ff7539bcd1da1
Maykal Tenev <michaeltenev@gmail.com>
2024-10-20 23:56:16 +0200

    03_ZigZagArrays

---
+++ 03_Arrays/03_Exercise/03_ZigZagArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+int n = int.Parse(Console.ReadLine());
+int[] array1 = new int[n];
+int[] array2 = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    int firstNum = int.Parse(input[0]);
+    int secondNum = int.Parse(input[1]);
+
+    if (i % 2 == 0)
+    {
+        array1[i] = firstNum;
+        array2[i] = secondNum;
+    }
+    else
+    {
+        array1[i] = secondNum;
+        array2[i] = firstNum;
+    }
+}
+
+Console.WriteLine(string.Join(" ", array1));
+Console.WriteLine(string.Join(" ", array2));
