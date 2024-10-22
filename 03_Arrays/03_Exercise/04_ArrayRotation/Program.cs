fd41239ef224538678b4de63b25d649bd07c75d4
Maykal Tenev <michaeltenev@gmail.com>
2024-10-22 19:16:12 +0200

    05_TopInteger

--- 03_Arrays/03_Exercise/04_ArrayRotation/Program.cs
+++ 03_Arrays/03_Exercise/04_ArrayRotation/Program.cs
@@ -8,16 +8,15 @@ class Program
         int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
         
         int rotations = int.Parse(Console.ReadLine());
-        rotations %= numbers.Length; 
-
+        rotations %= numbers.Length;
         for (int i = 0; i < rotations; i++)
         {
             int firstNumber = numbers[0];
-            for (int j = 0; j < numbers.Length - 1; j++) // Stop at the second-to-last element
+            for (int j = 0; j < numbers.Length - 1; j++)
             {
                 numbers[j] = numbers[j + 1];
             }
-            numbers[numbers.Length - 1] = firstNumber; // Move the first element to the end
+            numbers[numbers.Length - 1] = firstNumber; 
         }
 
         Console.WriteLine(string.Join(" ", numbers));
