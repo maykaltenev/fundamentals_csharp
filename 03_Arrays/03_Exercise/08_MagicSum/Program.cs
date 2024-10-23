aec756c31d183e34c38c299a65284a7a9fc5616b
Maykal Tenev <michaeltenev@gmail.com>
2024-10-23 15:57:47 +0200

    08_MagicSum

---
+++ 03_Arrays/03_Exercise/08_MagicSum/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+int magicSum = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    int firstNumber = numbers[i];
+    for (int j = i + 1; j < numbers.Length; j++)
+    {
+        int secondNumber = numbers[j];
+
+        if (firstNumber + secondNumber == magicSum)
+        {
+            Console.WriteLine($"{firstNumber} {secondNumber}");
+        }
+    }
+}
