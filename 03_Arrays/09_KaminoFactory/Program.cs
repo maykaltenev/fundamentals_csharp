43018785504be192e53ad92f3c10137c6804975a
Maykal Tenev <michaeltenev@gmail.com>
2024-10-23 17:47:21 +0200

    09_KaminoFactory and 10_LadyBug

---
+++ 03_Arrays/09_KaminoFactory/Program.cs
@@ -0,0 +1,85 @@
+﻿// Read the length of the DNA sequence
+int dnaLength = int.Parse(Console.ReadLine());
+
+string input;
+int bestSequenceIndex = 0;
+int bestSequenceSum = 0;
+int bestSubsequenceLength = 0;
+int bestSubsequenceStartIndex = int.MaxValue;
+int[] bestDnaSequence = new int[dnaLength];
+
+int sequenceIndex = 0;
+
+// Process each sequence until "Clone them!" is received
+while ((input = Console.ReadLine()) != "Clone them!")
+{
+    // Split the sequence by "!" and parse it into an integer array
+    int[] currentSequence = input
+        .Split(new char[] { '!' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+    sequenceIndex++;
+
+    int currentSequenceSum = currentSequence.Sum();
+    int currentSubsequenceLength = 0;
+    int currentSubsequenceStartIndex = 0;
+
+    int tempSubsequenceLength = 0;
+    int tempSubsequenceStartIndex = 0;
+
+    for (int i = 0; i < dnaLength; i++)
+    {
+        if (currentSequence[i] == 1)
+        {
+            tempSubsequenceLength++;
+            if (tempSubsequenceLength == 1)
+            {
+                tempSubsequenceStartIndex = i;
+            }
+
+            if (tempSubsequenceLength > currentSubsequenceLength)
+            {
+                currentSubsequenceLength = tempSubsequenceLength;
+                currentSubsequenceStartIndex = tempSubsequenceStartIndex;
+            }
+        }
+        else
+        {
+            tempSubsequenceLength = 0;
+        }
+    }
+
+    bool isBetterSequence = false;
+
+    if (currentSubsequenceLength > bestSubsequenceLength)
+    {
+        isBetterSequence = true;
+    }
+    else if (currentSubsequenceLength == bestSubsequenceLength)
+    {
+        if (currentSubsequenceStartIndex < bestSubsequenceStartIndex)
+        {
+            isBetterSequence = true;
+        }
+        else if (currentSubsequenceStartIndex == bestSubsequenceStartIndex)
+        {
+            if (currentSequenceSum > bestSequenceSum)
+            {
+                isBetterSequence = true;
+            }
+        }
+    }
+
+    if (isBetterSequence)
+    {
+        bestSequenceIndex = sequenceIndex;
+        bestSequenceSum = currentSequenceSum;
+        bestSubsequenceLength = currentSubsequenceLength;
+        bestSubsequenceStartIndex = currentSubsequenceStartIndex;
+        bestDnaSequence = currentSequence;
+    }
+}
+
+Console.WriteLine($"Best DNA sample {bestSequenceIndex} with sum: {bestSequenceSum}.");
+Console.WriteLine(string.Join(" ", bestDnaSequence));
---
+++ 03_Arrays/10_LadyBug/Program.cs
@@ -0,0 +1,38 @@
+﻿int fieldSize = int.Parse(Console.ReadLine());
+int[] field = new int[fieldSize];
+int[] ladybugsIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+foreach (int index in ladybugsIndexes)
+{
+    if (index >= 0 && index < fieldSize)
+    {
+        field[index] = 1;
+    }
+}
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] commandParts = command.Split();
+    int ladybugIndex = int.Parse(commandParts[0]);
+    string direction = commandParts[1];
+    int flyLength = int.Parse(commandParts[2]);
+
+    if (ladybugIndex < 0 || ladybugIndex >= fieldSize || field[ladybugIndex] == 0)
+    {
+        continue;
+    }
+    field[ladybugIndex] = 0;
+    int movement = direction == "right" ? 1 : -1;
+    int newPosition = ladybugIndex + (movement * flyLength);
+
+    while (newPosition >= 0 && newPosition < fieldSize && field[newPosition] == 1)
+    {
+        newPosition += (movement * flyLength);
+    }
+
+    if (newPosition >= 0 && newPosition < fieldSize)
+    {
+        field[newPosition] = 1;
+    }
+}
+Console.WriteLine(string.Join(" ", field));
