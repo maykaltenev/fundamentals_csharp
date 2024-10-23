c17a85926e4cccb4ddf930a3f8dac24e6a1ef5f3
Maykal Tenev <michaeltenev@gmail.com>
2024-10-23 15:29:21 +0200

    06_EqualSums and 07_MaxSequence

---
+++ 03_Arrays/03_Exercise/07_MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+int maxCount = 1;
+int currentCount = 1;
+int number = numbers[0];
+for (int i = 1; i < numbers.Length; i++)
+{
+    int previousNumber = numbers[i - 1];
+    int currentNumber = numbers[i];
+
+    if (previousNumber == currentNumber)
+    {
+        currentCount++;
+        if (currentCount > maxCount)
+        {
+            maxCount = currentCount;
+        }
+    }
+    else
+    {
+        currentCount = 1;
+    }
+}
+
+for (int i = 0; i < maxCount; i++)
+{
+    Console.Write(number + " ");
+}
+Console.WriteLine();
