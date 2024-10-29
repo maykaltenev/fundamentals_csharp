f05209e44cbffc381842c080a159e92d8b69b054
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 19:37:03 +0100

    05_Orders

--- 04_Methods/04_Labs/04_Printing/Program.cs
+++ 04_Methods/04_Labs/04_Printing/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        PrintTriangle(number);
+        PrintReverseTriangle(number);
+    }
+
+    static void PrintTriangle(int maxNumber)
+    {
+        for (int row = 1; row <= maxNumber; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void PrintReverseTriangle(int maxNumber)
+    {
+        for (int row = maxNumber - 1; row > 0; row--)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write(col + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 04_Methods/04_Labs/05_Orders/Program.cs
+++ 04_Methods/04_Labs/05_Orders/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string input = Console.ReadLine();
+int quantity = int.Parse(Console.ReadLine());
+Order(input, quantity);
+static void Order(string input, int quantity)
+{
+    double price = 0;
+    if (input == "coffee")
+    {
+        price = 1.50;
+    }
+    else if (input == "water")
+    {
+        price = 1.00;
+    }
+    else if (input == "coke")
+    {
+        price = 1.40;
+    }
+    else if (input == "snacks")
+    {
+        price = 2.00;
+    }
+    Console.WriteLine($"{(price * quantity):f2}");
+}
