072cd6b23fe65d7bd3a3bd5b6f91c9ff4f0a505d
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 19:45:22 +0100

    07_RepeatString

--- 04_Methods/04_Labs/07_RepeatString/Program.cs
+++ 04_Methods/04_Labs/07_RepeatString/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int count = int.Parse(Console.ReadLine());
+        string result = RepeatString(input, count);
+        Console.WriteLine(result);
+    }
+
+    private static string RepeatString(string str, int repeat)
+    {
+        string result = "";
+        for (int i = 0; i < repeat; i++)
+        {
+            result += str;
+        }
+        return result;
+    }
+}
