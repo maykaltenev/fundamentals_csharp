818b0164211539b0a09ec8b997a7ff484d686161
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 20:25:59 +0100

    11_MathOperations

---
+++ 04_Methods/04_Labs/11_MathOperations/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int first = int.Parse(Console.ReadLine());
+        string action = Console.ReadLine();
+        int second = int.Parse(Console.ReadLine());
+        double result = Calculate(first, action, second);
+        Console.WriteLine(result);
+    }
+
+    public static double Calculate(int first, string action, int second)
+    {
+        double result = 0;
+
+        switch (action)
+        {
+            case "/":
+                result = first / second;
+                break;
+            case "+":
+                result = first + second;
+                break;
+            case "*":
+                result = first * second;
+                break;
+            case "-":
+                result = first - second;
+                break;
+        }
+        return result;
+    }
+}
