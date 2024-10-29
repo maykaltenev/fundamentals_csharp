3e02580b212d6bd277c09a90f8719972a575a714
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 20:19:40 +0100

    10_MultiplyEvensByOdds

--- 04_Methods/04_Labs/09_GreaterOfTwoValue/Program.cs
+++ 04_Methods/04_Labs/09_GreaterOfTwoValue/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string type = Console.ReadLine();
+
+        if (type == "int")
+        {
+            int firstValue = int.Parse(Console.ReadLine());
+            int secondValue = int.Parse(Console.ReadLine());
+            int result = GetMax(firstValue, secondValue);
+            Console.WriteLine(result);
+        }
+        else if (type == "char")
+        {
+            char firstValue = Console.ReadLine()[0];
+            char secondValue = Console.ReadLine()[0];
+            char result = GetMax(firstValue, secondValue);
+            Console.WriteLine(result);
+        }
+        else if (type == "string")
+        {
+            string firstValue = Console.ReadLine();
+            string secondValue = Console.ReadLine();
+            string result = GetMax(firstValue, secondValue);
+            Console.WriteLine(result);
+        }
+    }
+
+    private static int GetMax(int value, int secondValue)
+    {
+        return value > secondValue ? value : secondValue;
+    }
+
+    private static char GetMax(char value, char secondValue)
+    {
+        return value > secondValue ? value : secondValue;
+    }
+
+    private static string GetMax(string value, string secondValue)
+    {
+        return string.Compare(value, secondValue) > 0 ? value : secondValue;
+    }
+}
--- 04_Methods/04_Labs/10_MultiplyEvensByOdds/Program.cs
+++ 04_Methods/04_Labs/10_MultiplyEvensByOdds/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        (int[] evens, int[] odds) = GetMultipleOfEvenAndOdds(number);
+
+        int sumOfEvenDigits = GetSumOfEvenDigits(evens);
+        int sumOfOddDigits = GetSumOfOddDigits(odds);
+        Console.WriteLine(sumOfEvenDigits * sumOfOddDigits);
+    }
+
+    private static (int[] evens, int[] odds) GetMultipleOfEvenAndOdds(int number)
+    {
+        string numberString = Math.Abs(number).ToString();
+        int[] evens = new int[numberString.Length];
+        int[] odds = new int[numberString.Length];
+
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < numberString.Length; i++)
+        {
+            int digit = numberString[i] - '0';
+            if (digit % 2 == 0)
+            {
+                evens[evenCount++] = digit;
+            }
+            else
+            {
+                odds[oddCount++] = digit;
+            }
+        }
+        Array.Resize(ref evens, evenCount);
+        Array.Resize(ref odds, oddCount);
+        return (evens, odds);
+    }
+
+    private static int GetSumOfEvenDigits(int[] evens)
+    {
+        return evens.Sum();
+    }
+
+    private static int GetSumOfOddDigits(int[] odds)
+    {
+        return odds.Sum();
+    }
+}
