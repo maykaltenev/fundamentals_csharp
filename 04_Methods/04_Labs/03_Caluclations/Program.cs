9f6982c43c6ea16b6f9fc061ead6bc40e711f17e
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 15:37:06 +0100

    03_Calculations

--- 04_Methods/04_Labs/03_Caluclations/Program.cs
+++ 04_Methods/04_Labs/03_Caluclations/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string command = Console.ReadLine();
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+switch (command)
+{
+    case "add":
+        Add(a, b);
+        break;
+    case "subtract":
+        Subtract(a, b);
+        break;
+    case "multiply":
+        Multiply(a, b);
+        break;
+    case "divide":
+        Divide(a, b);
+        break;
+}
+static void Divide(int a, int b)
+{
+    Console.WriteLine(a / b);
+}
+static void Multiply(int a, int b)
+{
+    Console.WriteLine(a * b);
+}
+static void Add(int a, int b)
+{
+    Console.WriteLine(a + b);
+}
+static void Subtract(int a, int b)
+{
+    Console.WriteLine(a - b);
+}
