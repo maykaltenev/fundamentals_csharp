30f1f5d6ea9c15031944557eb38dc6145a69ee1a
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 19:49:24 +0100

    08_MathPow

--- 04_Methods/04_Labs/08_MathPower/Program.cs
+++ 04_Methods/04_Labs/08_MathPower/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numberOne = double.Parse(Console.ReadLine());
+        double numberTwo = double.Parse(Console.ReadLine());
+        double result = RaiseToPower(numberOne, numberTwo);
+        Console.WriteLine(result);
+    }
+
+    private static double RaiseToPower(double first, double second)
+    {
+        return Math.Pow(first, second);
+    }
+}
