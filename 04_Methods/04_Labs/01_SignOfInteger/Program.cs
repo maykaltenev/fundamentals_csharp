7b50640bf2f0caa44885842384343068fea48506
Maykal Tenev <michaeltenev@gmail.com>
2024-10-29 15:21:19 +0100

    01_singOfInteger

---
+++ 04_Methods/04_Labs/01_SignOfInteger/Program.cs
@@ -0,0 +1,17 @@
+﻿PrintSign(int.Parse(Console.ReadLine()));
+
+static void PrintSign(int number)
+{
+    if (number < 0)
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else if (number > 0)
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else if (number == 0)
+    {
+        Console.WriteLine($"The number {number} is zero.");
+    }
+}
