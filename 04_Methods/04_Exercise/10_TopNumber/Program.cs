91321c21baa1bcd1bac8fd7c002e7f821580378f
Maykal Tenev <michaeltenev@gmail.com>
2024-10-30 17:38:24 +0100

    10_TopNumber

---
+++ 04_Methods/04_Exercise/10_TopNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+{
+    if (IsTopNumber(i))
+    {
+        Console.WriteLine(i);
+    }
+}
+
+bool IsTopNumber(int i)
+{
+    int sumOfDigits = 0;
+    bool hasOddDigit = false;
+    int currentNumber = i;
+
+    while (currentNumber > 0)
+    {
+        int digit = currentNumber % 10;
+        sumOfDigits += digit;
+
+        if (digit % 2 != 0)
+        {
+            hasOddDigit = true;
+        }
+        currentNumber /= 10;
+    }
+    return (sumOfDigits % 8 == 0) && hasOddDigit;
+}
