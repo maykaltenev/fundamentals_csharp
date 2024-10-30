67dbd04ef96ab667d0b127ddf60d03011e5fd461
Maykal Tenev <michaeltenev@gmail.com>
2024-10-30 14:29:46 +0100

    01_SmallestOfThreNumbers

--- 04_Methods/04_Exercise/01_SmallestOfThreeNumbers/Program.cs
+++ 04_Methods/04_Exercise/01_SmallestOfThreeNumbers/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+int smallest = GetSmallestNumber(firstNumber, secondNumber, thirdNumber);
+Console.WriteLine(smallest);
+int GetSmallestNumber(int firstNumber, int secondNumber, int thirdNumber)
+{
+    int smallest = int.MaxValue;
+    int[] allNumbers = { firstNumber, secondNumber, thirdNumber };
+    for (int i = 0; i < allNumbers.Length; i++)
+    {
+        if (allNumbers[i] < smallest)
+        {
+            smallest = allNumbers[i];
+        }
+    }
+    return smallest;
+}
