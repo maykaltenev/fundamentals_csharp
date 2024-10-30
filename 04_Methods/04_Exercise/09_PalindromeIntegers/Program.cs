96802a153c7d2bfa1afd49f06b69ca79e30e4c34
Maykal Tenev <michaeltenev@gmail.com>
2024-10-30 17:29:49 +0100

    09_PalindromeIntegers

---
+++ 04_Methods/04_Exercise/09_PalindromeIntegers/Program.cs
@@ -0,0 +1,18 @@
+﻿CheckPalindrome();
+void CheckPalindrome()
+{
+    string input = Console.ReadLine();
+    while (input != "END")
+    {
+        string reversed = new string(input.Reverse().ToArray());
+        if (input == reversed)
+        {
+            Console.WriteLine("true");
+        }
+        else
+        {
+            Console.WriteLine("false");
+        }
+        input = Console.ReadLine();
+    }
+}
