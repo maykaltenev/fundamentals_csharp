afdae92b2d5bed1ab67073d83838faec19b23560
Maykal Tenev <michaeltenev@gmail.com>
2024-10-30 15:05:42 +0100

    03_CharacterInRange

--- 04_Methods/04_Exercise/03_CharacterInRange/Program.cs
+++ 04_Methods/04_Exercise/03_CharacterInRange/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char start = Console.ReadLine()[0];
+char end = Console.ReadLine()[0];
+GetCharactersInRange(start, end);
+
+void GetCharactersInRange(char start, char end)
+{
+    for (char i = (char)(start + 1); i < end; i++)
+    {
+        Console.Write(i + " ");
+    }
+}
