b0da031080093666a640e4927d4cc18a329390a3
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:49:11 +0100

    06_ListManipulation

---
+++ 05_Lists/05_Labs/06_ListManipulationBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿List<int> items = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+while (true)
+{
+    string input = Console.ReadLine();
+    if (input == "end")
+    {
+        break;
+    }
+    string[] tokens = input.Split(' ');
+    switch (tokens[0])
+    {
+        case "Add":
+            items.Add(int.Parse(tokens[1]));
+            break;
+        case "Remove":
+            items.Remove(int.Parse(tokens[1]));
+            break;
+        case "RemoveAt":
+            items.RemoveAt(int.Parse(tokens[1]));
+            break;
+        case "Insert":
+            items.Insert(int.Parse(tokens[2]), int.Parse(tokens[1]));
+            break;
+    }
+}
+Console.WriteLine(string.Join(" ", items));
