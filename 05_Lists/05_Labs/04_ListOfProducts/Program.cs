d66e123c714b4317f8fe7b01eaf4d1494a8d423a
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:26:46 +0100

    05_ListOfProducts

---
+++ 05_Lists/05_Labs/04_ListOfProducts/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = int.Parse(Console.ReadLine());
+List<string> items = new List<string>(n);
+for (int i = 0; i < n; i++)
+{
+    string product = Console.ReadLine();
+    items.Add(product);
+}
+items.Sort();
+for (int i = 0; i < items.Count; i++)
+{
+    Console.WriteLine($"{i + 1}.{items[i]}");
+}
