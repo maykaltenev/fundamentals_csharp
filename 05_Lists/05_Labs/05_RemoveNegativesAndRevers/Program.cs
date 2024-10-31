ab951ba9a0bf5ca58f379ca70a892e220ba9e38e
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:29:56 +0100

    05_RemoveAllNegative

---
+++ 05_Lists/05_Labs/05_RemoveNegativesAndRevers/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> items = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+items.RemoveAll(n => n < 0);
+if (items.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    Console.WriteLine(string.Join(" ", items));
+}
