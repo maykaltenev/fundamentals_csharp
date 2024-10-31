dbef29b8c64ee50d9b5a62b534351d1e004b7fce
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:21:46 +0100

    03_MergingLists

---
+++ 05_Lists/05_Labs/03_MergingLists/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> firstList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> secondList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> result = new List<int>();
+
+for (var i = 0; i < Math.Min(firstList.Count, secondList.Count); i++)
+{
+    result.Add(firstList[i]);
+    result.Add(secondList[i]);
+}
+result.AddRange(firstList.Skip(result.Count / 2));
+result.AddRange(secondList.Skip(result.Count / 2));
+Console.WriteLine(string.Join(" ", result));
