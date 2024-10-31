3da94ef31961bda99e9fa2b9448145ab46bae305
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:14:16 +0100

    02_GaussTrick

---
+++ 05_Lists/05_Labs/02_GaussTrick/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+List<int> result = new List<int>();
+
+int start = 0;
+int end = numbers.Count - 1;
+
+while (start <= end)
+{
+    if (start == end)
+    {
+        result.Add(numbers[start]);
+    }
+    else
+    {
+        result.Add(numbers[start] + numbers[end]);
+    }
+    start++;
+    end--;
+}
+Console.WriteLine(string.Join(" ", result));
