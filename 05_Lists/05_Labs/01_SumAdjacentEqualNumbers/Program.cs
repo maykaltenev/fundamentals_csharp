0096cc7986ef4f660fd92b5cab7b5e511c53688d
Maykal Tenev <michaeltenev@gmail.com>
2024-10-31 17:09:02 +0100

    01_SumAdjacentEqualNumbers

---
+++ 05_Lists/05_Labs/01_SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿List<double> items = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+for (int i = 0; i < items.Count - 1; i++)
+{
+    if (items[i] == items[i + 1])
+    {
+        items[i] += items[i + 1];
+        items.RemoveAt(i + 1);
+        i = -1;
+    }
+}
+Console.WriteLine(string.Join(" ", items));
