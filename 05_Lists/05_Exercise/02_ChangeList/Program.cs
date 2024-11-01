746b8c9298267e6e60794651aad8ddd6394d385f
Maykal Tenev <michaeltenev@gmail.com>
2024-11-01 15:40:41 +0100

    02_ChangeList

---
+++ 05_Lists/05_Exercise/02_ChangeList/Program.cs
@@ -0,0 +1,18 @@
+﻿List<int> items = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+string command;
+
+while ((command = Console.ReadLine()) != "end")
+{
+    string[] tokens = command.Split(' ');
+    if (tokens[0] == "Delete")
+    {
+        items.RemoveAll(x => x == int.Parse(tokens[1]));
+    }
+    else if (tokens[0] == "Insert")
+    {
+        int element = int.Parse(tokens[1]);
+        int position = int.Parse(tokens[2]);
+        items.Insert(position, element);
+    }
+}
+Console.WriteLine(string.Join(" ", items));
