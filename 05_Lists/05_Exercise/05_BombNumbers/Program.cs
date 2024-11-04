b4b05c04d43a9399230be8750cf3200570d1ab90
Maykal Tenev <michaeltenev@gmail.com>
2024-11-04 15:08:38 +0100

    05_BombNumbers

--- 05_Lists/05_Exercise/05_BombNumbers/Program.cs
+++ 05_Lists/05_Exercise/05_BombNumbers/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+List<int> specialNumberAndPower = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+int specialNumber = specialNumberAndPower[0];
+int power = specialNumberAndPower[1];
+List<int> indexes = new List<int>();
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (numbers[i] == specialNumber)
+    {
+        int startIndex = Math.Max(0, i - power);
+        int endIndex = Math.Min(numbers.Count - 1, i + power);
+
+        for (int j = endIndex; j >= startIndex; j--)
+        {
+            numbers.RemoveAt(j);
+        }
+    }
+}
+int sum = 0;
+for (int i = 0; i < numbers.Count; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine("" + sum);
