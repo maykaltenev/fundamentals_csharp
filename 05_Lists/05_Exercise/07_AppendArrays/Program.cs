eb77154e57aeb2bee5238833db72bcc95ba09352
Maykal Tenev <michaeltenev@gmail.com>
2024-11-05 12:53:01 +0100

    07_AppendArrays

---
+++ 05_Lists/05_Exercise/07_AppendArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+var arrays = input.Split('|').Select(arr => arr.Trim()).Reverse().ToArray();
+var result = new List<string>();
+
+foreach (var array in arrays)
+{
+    var numbers = array.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    result.AddRange(numbers);
+}
+Console.WriteLine(string.Join(" ", result));
